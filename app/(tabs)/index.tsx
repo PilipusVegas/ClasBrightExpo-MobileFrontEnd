005c568722ca3f598aded0e5337bb3c71fe2bf4a
PRDV22 <53340059+PilipusVegas@users.noreply.github.com>
2024-10-29 10:25:40 +0700

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { JumboTabs } from 'antd-mobile';
-import { SetOutline, BellOutline, LeftOutline, RightOutline, SearchOutline, UserCircleOutline, UnorderedListOutline } from 'antd-mobile-icons';
+import { useNavigate } from 'react-router-dom';
+import { SetOutline, BellOutline, LeftOutline, TextOutline, RightOutline, SearchOutline, UserCircleOutline, UnorderedListOutline } from 'antd-mobile-icons';
 
 const dataList = [
   { id: 1, po: 'PO001', tanggal: '2024-10-01', jumlah: 10 },
@@ -16,6 +17,7 @@ const dataList = [
 ];
 
 export default () => {
+  const navigate = useNavigate();
   const [activeKey, setActiveKey] = useState('1');
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -33,17 +35,17 @@ export default () => {
       return (
         <div style={{ marginTop: '10px' }}>
           <div style={{ width: '100%', display: 'flex', position: 'relative' }}>
-            <SearchOutline style={{ top: '50%', left: '10px', color: '#00A19D', fontSize: '24px', position: 'absolute', transform: 'translateY(-50%)' }} />
-            <input type="text" placeholder="Cari NO. PO ..." value={searchTerm.toUpperCase()} onChange={e => setSearchTerm(e.target.value.toUpperCase())} style={{ width: '100%', color: '#00A19D', borderRadius: '10px', textTransform: 'uppercase', border: '1px solid #00A19D', padding: '10px 10px 10px 40px' }} />
-            <style>{`input::placeholder {color: #A0A0A0; // Ganti dengan warna abu-abu yang diinginkan}`}</style>
-        </div>
-          <div style={{ maxHeight: '500px', overflowY: 'auto' }}>
+            <SearchOutline style={{ top: '45%', left: '10px', color: '#00A19D', fontSize: '20px', position: 'absolute', transform: 'translateY(-50%)' }} />
+            <input type="text" placeholder="Cari NO. PO ..." value={searchTerm.toUpperCase()} onChange={e => setSearchTerm(e.target.value.toUpperCase())} style={{ width: '100%', color: '#00A19D', borderRadius: '15px', textTransform: 'uppercase', border: '1px solid #00A19D', padding: '10px 10px 10px 40px' }} />
+            <style>{`input::placeholder {color: #A0A0A0 }`}</style>
+          </div>
+          <div style={{ overflowY: 'auto', maxHeight: '500px' }}>
             {filteredData.map(item => (
-              <div key={item.id} style={{ padding: '30px', margin: '10px 0', position: 'relative', borderRadius: '10px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D' }}>
+              <div key={item.id} onClick={() => navigate(`/podetail/${item.id}`)} style={{ padding: '30px', margin: '10px 0', position: 'relative', borderRadius: '15px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D' }}>
                 <div style={{ top: '10px', left: '10px', color: '#00A19D', fontWeight: 'bold', position: 'absolute' }}>{item.po}</div>
                 <div style={{ top: '10px', right: '10px', color: '#00A19D', position: 'absolute' }}>{formatDate(item.tanggal)}</div>
                 <div style={{ left: '10px', bottom: '10px', color: '#00A19D', position: 'absolute' }}>Jumlah: {item.jumlah}</div>
-                <div style={{ right: '10px', bottom: '10px', padding: '2px', color: '#FFFFFF', borderRadius: '5px', position: 'absolute', backgroundColor: '#00A19D' }}>Pick Up <RightOutline style={{ marginLeft: '5px' }} /></div>
+                <div style={{ right: '10px', bottom: '10px', padding: '2px', color: '#00A19D', position: 'absolute', borderRadius: '15px', border: '1px solid #00A19D' }}>Pick Up <RightOutline /></div>
               </div>
             ))}
           </div>
@@ -54,25 +56,26 @@ export default () => {
   };
 
   return (
-    <div>
       <div style={{ width: '95%', margin: '0 auto' }}>
-        <div style={{ padding: '15px', color: '#ffffff', fontSize: '24px', marginTop: '10px', fontWeight: 'bold', borderRadius: '8px', backgroundColor: '#00A19D', border: '1px solid #00A19D' }}>Beranda</div>
-        <div style={{ padding: '10px', color: '#00A19D', fontSize: '16px', marginTop: '10px', borderRadius: '10px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D' }}>
-          <div style={{ display: 'flex', fontSize: '16px', alignItems: 'center' }}>
-            <UserCircleOutline style={{ marginRight: '10px', fontSize: '30px' }} />Selamat Datang, User
+        <div style={{ display: 'flex', padding: '15px', color: '#00A19D', fontSize: '24px', marginTop: '10px', fontWeight: 'bold', alignItems: 'center', justifyContent: 'space-between' }}>
+          <span style={{ marginLeft: '-10px' }}>Beranda</span>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <SetOutline style={{ fontSize: '30px', cursor: 'pointer', marginRight: '10px' }} />
+            <LeftOutline style={{ fontSize: '30px', cursor: 'pointer', marginRight: '-20px' }} />
           </div>
         </div>
-        <div style={{ color: '#00A19D', marginTop: '10px', borderRadius: '10px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D', overflow: 'hidden' }}>
+        <div style={{ padding: '10px', color: '#00A19D', fontSize: '16px', marginTop: '10px', borderRadius: '15px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D' }}>
+          <div style={{ display: 'flex', fontSize: '16px', alignItems: 'center' }}>Selamat Datang, User</div>
+        </div>
+        <div style={{ color: '#00A19D', marginTop: '10px', overflow: 'hidden', borderRadius: '15px', backgroundColor: '#FFFFFF', border: '1px solid #00A19D' }}>
           <JumboTabs defaultActiveKey="1" activeKey={activeKey} onChange={key => setActiveKey(key)}>
             <JumboTabs.Tab key="1" title={<UnorderedListOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "1" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Beranda</span>} />
-            <JumboTabs.Tab key="2" title={<BellOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "2" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Notifikasi</span>} />
-            <JumboTabs.Tab key="3" title={<UserCircleOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "3" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Profil</span>} />
-            <JumboTabs.Tab key="4" title={<SetOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "4" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Pengaturan</span>} />
-            <JumboTabs.Tab key="5" title={<LeftOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "5" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Keluar</span>} />
+            <JumboTabs.Tab key="2" title={<TextOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "2" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>History</span>} />
+            <JumboTabs.Tab key="3" title={<BellOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "3" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Notifikasi</span>} />
+            <JumboTabs.Tab key="4" title={<UserCircleOutline style={{ fontSize: '30px' }} />} description={<span style={{ color: activeKey === "4" ? '#FFFFFF' : '#00A19D', fontSize: '16px' }}>Profil</span>} />
           </JumboTabs>
         </div>
         {renderContent()}
       </div>
-    </div>
   );
 };
