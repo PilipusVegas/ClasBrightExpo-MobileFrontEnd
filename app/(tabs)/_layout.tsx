2bc44440df4deaed96b0a6d4a99e44a8f7e233ed
PRDV22 <53340059+PilipusVegas@users.noreply.github.com>
2024-10-31 08:58:03 +0700

    Update _layout.tsx
    - menambahkan route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import Dashboard from './index';
+import PoDetail from './PoDetail';
 import LoginScreen from './LoginScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <View>
-      <LoginScreen />
-    </View>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/beranda" element={<Dashboard />} />
+        <Route path="/podetail/:id_po" element={<PoDetail />} />
+      </Routes>
+    </Router>
   );
 }
