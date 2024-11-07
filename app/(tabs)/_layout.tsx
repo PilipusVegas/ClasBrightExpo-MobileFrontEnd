ccc7af65911c39b3e7ff32bbe5037f86972905c1
PRDV22 <53340059+PilipusVegas@users.noreply.github.com>
2024-11-07 16:38:54 +0700

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import Login from './Login';
 import Dashboard from './index';
 import PoDetail from './PoDetail';
-import LoginScreen from './LoginScreen';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
+        <Route path="/" element={<Login />} />
         <Route path="/beranda" element={<Dashboard />} />
         <Route path="/podetail/:id_po" element={<PoDetail />} />
       </Routes>
